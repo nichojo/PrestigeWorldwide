d2ba34bed469a68a3ab688cfb890f53bb15262d4
nichojo <nichols.jody@gmail.com>
2017-04-27 16:18:11 +1000

    Add project files.

---
+++ PrestigeWorldwide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrestigeWorldwide.Startup))]
+namespace PrestigeWorldwide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
